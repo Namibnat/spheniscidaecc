64b885ebf5dca820d85824c887c3775755bbe419
Vernon Swanepoel <frantic.naturalist@gmail.com>
2020-12-11 13:47:18 +0000

    Open and read a source file

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 /* The compiler */
+#include <stdio.h>
 
 int main(int argc, char *argv[]){
-	
-	char *program = "int main(){\nreturn 0\n}";
+	int c;
+
+	FILE *source_fp;
+	source_fp = fopen("test.c", "r");
+
+	if(source_fp){
+		while ((c = getc(source_fp)) != EOF){
+			putchar(c);
+		}
+	}
+	else{
+		printf("Reading file \"test.c\" failed");
+	}
+	fclose(source_fp);
 
 	return 0;
 }
