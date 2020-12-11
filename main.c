eb46c26afed575756a117c85552c7243ee5a17e4
Vernon Swanepoel <frantic.naturalist@gmail.com>
2020-12-11 14:27:26 +0000

    Read a single source file from cmd line arg
    If the program to be compiled is supplied by the user
    on the command line, it gets opened, otherwise
    the program fails.

--- main.c
+++ main.c
@@ -1,21 +1,37 @@
 /* The compiler */
+
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]){
+
 	int c;
+	int filenamesize;
+	char *filename;
 
-	FILE *source_fp;
-	source_fp = fopen("test.c", "r");
+	if(argc >= 2){
+		filenamesize = strlen(argv[1]);
+		filename = malloc(filenamesize * sizeof(char));
+		strcpy(filename, argv[1]);
+		FILE *source_fp;
+		source_fp = fopen(filename, "r");
 
-	if(source_fp){
-		while ((c = getc(source_fp)) != EOF){
-			putchar(c);
+		if(source_fp){
+			while ((c = getc(source_fp)) != EOF){
+				putchar(c);
+			}
+			fclose(source_fp);
+		}
+		else{
+			printf("Reading file \"test.c\" failed\n");
 		}
 	}
 	else{
-		printf("Reading file \"test.c\" failed");
+		printf("No file to compile provided\n");
 	}
-	fclose(source_fp);
+
+
 
 	return 0;
 }
