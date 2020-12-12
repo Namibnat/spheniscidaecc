7cab4c641b2e9431aab68f702ca7a094e58dc36b
Vernon Swanepoel <frantic.naturalist@gmail.com>
2020-12-12 16:27:10 +0000

    Find space characters between tokens

--- main.c
+++ main.c
@@ -2,12 +2,40 @@
 
 #include "spheniscidaecc.h"
 
+#define ISSPACE 1
+#define ISNOTSPACE 0
+
 char *source_code;
 
-static void tokenize(char *source){
-	for (size_t i; i < strlen(source); i++){
-		printf("mysource[%lu] = %c\n", i, source[i]);
+int is_space_char(char c){
+	switch(c){
+		case ' ':
+			return ISSPACE;
+			break;
+		case '\n':
+			return ISSPACE;
+			break;
+		case '\t':
+			return ISSPACE;
+			break;
+		default:
+			return ISNOTSPACE;
 	}
+}
+
+static void tokenize(char *source){
+	char intoken = 1;
+	for (size_t i; i < strlen(source); i++){
+		if(is_space_char(source[i])){
+			intoken = 0;
+			printf("; ");
+		}
+		else{
+			intoken = 1;
+			putchar(source[i]);
+		}
+	}
+	printf("\n");
 }
 
 static void parse_args(int argc, char **argv){
