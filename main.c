f9a4cc3f13b83b40d469838bf605b0e2d29a932d
Vernon Swanepoel <frantic.naturalist@gmail.com>
2020-12-14 08:30:42 +0000

    Minor cleanup with main & mem allocation

--- main.c
+++ main.c
@@ -12,13 +12,19 @@ static int parse_args(int argc, char **argv){
 	if(argc >= 2){
 		/* argv[2] here assumed to be the input file.
 		 * later on some error checking, mutiple files, etc
-		 * will get added.  */
+		 * will get added. 
+		 *
+		 * I'm copying the filename because I know in the end I'll
+		 * need to copy a few of them.
+		 */
 		filenamesize = strlen(argv[1]);
 		filename = malloc(filenamesize * sizeof(char));
 		strcpy(filename, argv[1]);
+
 		FILE *source_fp;
 		source_fp = fopen(filename, "rb");
 		if(!source_fp) perror(filename), exit(1);
+		free(filename);
 
 		fseek(source_fp, 0L, SEEK_END);
 		lSize = ftell(source_fp);
@@ -27,7 +33,7 @@ static int parse_args(int argc, char **argv){
 		source = calloc(1, lSize+1);
 		if(!source){
 		       	fclose(source_fp);
-			fputs("Memory alloc fails", stderr);
+			fputs("Memory allocation failed", stderr);
 		       	exit(1);
 		}
 		if(1 != fread(source, lSize, 1, source_fp)){
